f0681de156412c97e7477a47c08675e6b8d6f7dc
ElaineYao <elainedv111@gmail.com>
2020-12-14 18:16:51 +0800

    insert fi = ... automatically

--- importFi.py
+++ importFi.py
@@ -1,45 +1,65 @@
 import ast
 import astor
+import logging
 
 # TODO: Add some configuration in this process to workflow
-# TODO: Can the ast find the previous variable?
 
-source1 = '''
-fi = ti.TensorFI(sess,
-                configFileName = 'default.yaml',
-                logLevel = 50,
-                disableInjections=False,
-                name = "convolutional",
-                fiPrefix = 'fi_')
-'''
+def addFi(fPath, # Source file path
+          configFileName="confFiles/default.yaml",  # Config file for reading fault configuration
+          logDir="faultLogs/",  # Log directory for the Fault log (Not to be confused with the logging level below)
+          logLevel=logging.DEBUG,  # Logging level {DEBUG=10, INFO=20, ERROR=30}
+          disableInjections=False,  # Should we disable injections after instrumenting ?
+          name="NoName",  # The name of the injector, used in statistics and logging
+          fiPrefix="fi_"):  # Prefix to attach to each node inserted for fault injection
 
-source = '''
-fi = ti.TensorFI(sess)
-'''
+    parse_src = ast.parse(open(fPath).read())
+    s = 'tf.Session' # This is the session from tensorFlow
+    i=0 # Index in body node
+    j=0 # Index in with node
 
-sessName = ast.Name("sess", ast.Load())
-sessNameList = []
-sessNameList.append(sessName)
-tiName = ast.Name('ti', ast.Load())
-fiName = ast.Name('fi', ast.Store())
-fiNameList = []
-fiKeyList = []
-fiNameList.append(fiName)
-confKey = ast.keyword('configFileName', ast.Str('default.yaml'))
-logKey = ast.keyword('logLevel', ast.Num(50))
-disaKey = ast.keyword('disableInjections', ast.Name('False', ast.Load()))
-nameKey = ast.keyword('name', ast.Str('convolutional'))
-preKey = ast.keyword('fiPrefix', ast.Str('fi_'))
-fiKeyList.append(confKey)
-fiKeyList.append(logKey)
-fiKeyList.append(disaKey)
-fiKeyList.append(preKey)
-tiAttr = ast.Attribute(tiName, 'TensorFI', ast.Load())
-tiCall = ast.Call(tiAttr, sessNameList, fiKeyList, None, None)
-tiBody = ast.Assign(fiNameList, tiCall)
+    for node in ast.iter_child_nodes(parse_src):
+        i = i+1
+        # Find 'with tf.Session as sess', instead of other 'with' expressions
+        if isinstance(node, ast.With) and node.context_expr.func.attr == 'Session':
+            s = node.body[0].value.func.value.id
+            break
 
-# print(ast.dump(ast.parse(source1)))
-print(astor.to_source(tiBody))
+    withNode = parse_src.body[i-1]
+
+    for node in ast.walk(withNode):
+        j = j+1
+
+    sessName = ast.Name(s, ast.Load())
+    sessNameList = []
+    sessNameList.append(sessName)
+    tiName = ast.Name('ti', ast.Load())
+    fiName = ast.Name('fi', ast.Store())
+    fiNameList = []
+    fiKeyList = []
+    fiNameList.append(fiName)
+    confKey = ast.keyword('configFileName', ast.Str(configFileName))
+    logDKey = ast.keyword('logDir', ast.Str(logDir))
+    logKey = ast.keyword('logLevel', ast.Num(logLevel))
+    disaKey = ast.keyword('disableInjections', ast.Name(disableInjections, ast.Load()))
+    nameKey = ast.keyword('name', ast.Str(name))
+    preKey = ast.keyword('fiPrefix', ast.Str(fiPrefix))
+    fiKeyList.append(confKey)
+    fiKeyList.append(logDKey)
+    fiKeyList.append(logKey)
+    fiKeyList.append(disaKey)
+    fiKeyList.append(nameKey)
+    fiKeyList.append(preKey)
+    tiAttr = ast.Attribute(tiName, 'TensorFI', ast.Load())
+    tiCall = ast.Call(tiAttr, sessNameList, fiKeyList, None, None)
+    tiBody = ast.Assign(fiNameList, tiCall)
+
+    withNode.body.insert(j, tiBody)
+
+    # print(ast.dump(ast.parse(source1)))
+    print(astor.to_source(parse_src))
+
+if __name__ == '__main__':
+    addFi('sample.py','default.yaml',"faultLogs/", 50, 'False', 'convolutional', 'fi_')
 
 
 
