b98409d7db70e5d3202b485eb928188d1b488499
ElaineYao <elainedv111@gmail.com>
2020-12-14 15:42:57 +0800

    Realize inserting 'import TensorFI as ti' through a file

--- importTest.py
+++ importTest.py
@@ -1,24 +1,23 @@
 import ast
 import astor
 
-source = '''
-import os
-import tensorflow as tf
-'''
+def addImport(fPath):
 
+    i = 0;
+    parse_src = ast.parse(open(fPath).read())
 
-i = 0;
-parse_src = ast.parse(source)
+    for node in ast.walk(parse_src):
+        i = i+1;
+        if isinstance(node, ast.Import):
+            alia = ast.alias('TensorFI', 'ti')
+            aliaList = []
+            aliaList.append(alia)
+            importBody = ast.Import(aliaList)
+            parse_src.body.insert(i, importBody)
+            break
 
-for node in ast.walk(parse_src):
-    i = i+1;
-    if isinstance(node, ast.Import):
-        alia = ast.alias('TensorFI', 'ti')
-        aliaList = []
-        aliaList.append(alia)
-        importBody = ast.Import(aliaList)
-        parse_src.body.insert(i, importBody)
-        break
+    print(ast.dump(parse_src))
+    print(astor.to_source(parse_src))
 
-# print(ast.dump(parse_src))
-print(astor.to_source(parse_src))
+if __name__ == '__main__':
+    addImport('astTest.py')
