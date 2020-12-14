bb4a17c199a7faaf33da679fb381df6436a8ea1b
ElaineYao <elainedv111@gmail.com>
2020-12-14 15:17:39 +0800

    Ways to insert 'import TensorFI as ti' automatically

--- importTest.py
+++ importTest.py
@@ -1,16 +1,24 @@
 import ast
+import astor
 
-importSource = '''
-import TensorFI as ti
+source = '''
+import os
+import tensorflow as tf
 '''
 
-'''
-Module(body=[Import(names=[alias(name='Tensorfi', asname='ti')])])
-'''
-# Ways to insert 'import TensorFI as ti'
-alia = ast.alias(ast.Str('TensorFI'). ast.Str('ti'))
-importBody = ast.Import(alia)
 
-# TODO : find places automatically
+i = 0;
+parse_src = ast.parse(source)
 
-print(ast.dump(ast.parse(importBody)))
+for node in ast.walk(parse_src):
+    i = i+1;
+    if isinstance(node, ast.Import):
+        alia = ast.alias('TensorFI', 'ti')
+        aliaList = []
+        aliaList.append(alia)
+        importBody = ast.Import(aliaList)
+        parse_src.body.insert(i, importBody)
+        break
+
+# print(ast.dump(parse_src))
+print(astor.to_source(parse_src))
